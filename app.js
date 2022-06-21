64c630985909ea2e357ab70b4b88a250fdc508f7
Mikalai19 <bertman29@gmail.com>
2022-06-21 10:36:30 -0700

    added another example

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
-console.log(d3)
+// d3.select('div')
+//     .selectAll('p')
+//     .data([1, 2, 3])
+//     .enter()   // need it because we cannot bind the dato to unexisting element. enter() allows to create as much elements as wee need. For tjhis example 3 'p' elements
+//     .append('p')  // append new 'p' for every missing 'p'
+//     .text(dta => dta); //add text node to the elements 'p'
+// // first data gives you access to the each data point
+
+
+const SOME_DATA = [
+    { id: 'd1', value: 10, region: 'USA' },
+    { id: 'd2', value: 20, region: 'Belarus' },
+    { id: 'd3', value: 30, region: 'Ukraine' },
+    { id: 'd4', value: 40, region: 'Canada' }
+]
 
 
 d3.select('div')
     .selectAll('p')
-    .data([1, 2, 3])
-    .enter()   // need it because we cannot bind the dato to unexisting element. enter() allows to create as much elements as wee need. For tjhis example 3 'p' elements
-    .append('p')  // append new 'p' for every missing 'p'
-    .text(dta => dta);
+    .data(SOME_DATA)
+    .enter()
+    .append('p')
+    .text(dta => dta.region)
