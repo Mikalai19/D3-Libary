c5d6bbd4729261fdf7dbc2b12b96b9f896e41d11
Mikalai19 <bertman29@gmail.com>
2022-06-21 11:10:50 -0700

    adding style to the elements and made chart

--- app.js
+++ app.js
@@ -9,15 +9,31 @@
 
 const SOME_DATA = [
     { id: 'd1', value: 10, region: 'USA' },
-    { id: 'd2', value: 20, region: 'Belarus' },
-    { id: 'd3', value: 30, region: 'Ukraine' },
-    { id: 'd4', value: 40, region: 'Canada' }
+    { id: 'd2', value: 12, region: 'Belarus' },
+    { id: 'd3', value: 14, region: 'Ukraine' },
+    { id: 'd4', value: 16, region: 'Canada' }
 ]
 
 
-d3.select('div')
-    .selectAll('p')
+// d3.select('div')
+//     .selectAll('p')
+//     .data(SOME_DATA)
+//     .enter()
+//     .append('p')
+//     .text(dta => dta.region)
+
+
+const container = d3.select('div')
+    .classed('container', true)
+    .style('border', '1px solid red');
+
+
+const bar = container
+    .selectAll('.bar')
     .data(SOME_DATA)
     .enter()
-    .append('p')
-    .text(dta => dta.region)
+    .append('div')
+    .classed('bar', true)
+    .style('width', '50px')
+    .style('height', data => (data.value * 15) + 'px')
+
