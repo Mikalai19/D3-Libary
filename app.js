d0920b73a2a2f83d927097c66d7335f10c6b0bcf
Mikalai19 <bertman29@gmail.com>
2022-06-21 09:30:08 -0700

    basic steps to append data to elements

---
+++ app.js
@@ -0,0 +1,8 @@
+console.log(d3)
+
+
+d3.select('div')
+    .selectAll('p')
+    .data([1, 2, 3])
+    .enter()   // need it because we cannot bind the dato to unexisting element. enter() allows to create as much elements as wee need. For tjhis example 3 'p' elements
+    .append('p')  // append new 'p' for every missing 'p'
