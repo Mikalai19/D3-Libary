5fa21312f4807149f0892ffbd20c3ed0bb33cfa7
Mikalai19 <bertman29@gmail.com>
2022-06-21 09:35:24 -0700

    adding text to element p

--- app.js
+++ app.js
@@ -6,3 +6,4 @@ d3.select('div')
     .data([1, 2, 3])
     .enter()   // need it because we cannot bind the dato to unexisting element. enter() allows to create as much elements as wee need. For tjhis example 3 'p' elements
     .append('p')  // append new 'p' for every missing 'p'
+    .text(dta => dta);
